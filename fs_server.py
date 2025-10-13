65a2a3cd57444e2fdb13f8abfd4104295f8af4b5
Finlay Christ <randomiagaming@gmail.com>
2025-10-13 00:15:17 -0700

    Server is more stable to errors and auto recovers from crashed clients leaving files laying around.

--- fs_server.py
+++ fs_server.py
@@ -2,6 +2,7 @@
 
 import os
 import fcntl
+import time
 
 def RunServer(inputPath, outputPath, callback):
     # Start by deleting both input and output for a fresh start
@@ -12,53 +13,54 @@ def RunServer(inputPath, outputPath, callback):
     # Then run the main request processing loop
     print(f"Server is now waiting for input from \"{inputPath}\" and writting output to \"{outputPath}\".")
     while True:
-        # Wait for input to be created
-        while not os.path.exists(inputPath):
-            continue
-        # Wait for input to be size > 0
-        while not os.stat(inputPath).st_size > 0:
-            continue
-        # Open input
-        inputFD = os.open(inputPath, os.O_RDONLY, 0o644)
-        # Wait for lock on input file
-        fcntl.flock(inputFD, fcntl.LOCK_EX)
-        # Wrap input in python stream
-        inputFile = os.fdopen(inputFD, "r", encoding="UTF-8")
-        # Read input
-        inputStr = inputFile.read()
-        # Call callback
-        outputStr = callback(inputStr)
-        print(f"Client said \"{inputStr}\" responding with \"{outputStr}\".")
-        # Wait for output to be deleted
-        while os.path.exists(outputPath):
-            continue
-        # Create output
-        while True:
-            try:
-                outputFD = os.open(outputPath, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0o644)
-                break
-            except:
+        try:
+            # Wait for input to be created
+            while not os.path.exists(inputPath):
                 continue
-        # Lock output
-        fcntl.flock(outputFD, fcntl.LOCK_EX)
-        # Wrap output in python stream
-        outputFile = os.fdopen(outputFD, "w", encoding="UTF-8")
-        # Write output
-        outputFile.write(outputStr)
-        outputFile.flush()
-        # Unlock output
-        fcntl.flock(outputFD, fcntl.LOCK_UN)
-        # Close output
-        outputFile.close()
-        # Wait for output to be deleted for up to 10 seconds
-        while os.path.exists(outputPath):
-            continue
-        # Unlock input
-        fcntl.flock(inputFD, fcntl.LOCK_UN)
-        # Close input
-        inputFile.close()
-        # Delete input
-        os.unlink(inputPath)
+            # Wait for input to be size > 0
+            while not os.stat(inputPath).st_size > 0:
+                continue
+            # Open input
+            inputFD = os.open(inputPath, os.O_RDONLY, 0o644)
+            with os.fdopen(inputFD, "r", encoding="UTF-8") as inputFile:
+                # Wait for lock on input file
+                fcntl.flock(inputFD, fcntl.LOCK_EX)
+                # Read input
+                inputStr = inputFile.read()
+                # Call callback
+                outputStr = callback(inputStr)
+                print(f"Client said \"{inputStr}\" responding with \"{outputStr}\".")
+                # Wait for output to be deleted
+                while os.path.exists(outputPath):
+                    continue
+                # Create output
+                while True:
+                    try:
+                        outputFD = os.open(outputPath, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0o644)
+                        break
+                    except:
+                        continue
+                with os.fdopen(outputFD, "w", encoding="UTF-8") as outputFile:
+                    # Lock output
+                    fcntl.flock(outputFD, fcntl.LOCK_EX)
+                    # Write output
+                    outputFile.write(outputStr)
+                    outputFile.flush()
+                    # Unlock output
+                    fcntl.flock(outputFD, fcntl.LOCK_UN)
+                # Wait for output to be deleted for up to 5 seconds
+                startWaitTime = time.perf_counter()
+                while os.path.exists(outputPath):
+                    if time.perf_counter() - startWaitTime > 5.0:
+                        os.unlink(outputPath)
+                        break
+                    continue
+                # Unlock input
+                fcntl.flock(inputFD, fcntl.LOCK_UN)
+            # Delete input
+            os.unlink(inputPath)
+        except Exception as ex:
+            print(f"ERROR: {str(ex)}")
 
 def callback(inputStr):
     return f"You said {inputStr}"
